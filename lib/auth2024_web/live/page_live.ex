a940c7ad250522cb2c11e1e8568aa1793954dfbe
Timo Weggen <timo.weggen@rohde-schwarz.com>
2024-06-05 13:21:20 +0200

    - i think this is some steps forward.

--- lib/auth2024_web/live/page_live.ex
+++ lib/auth2024_web/live/page_live.ex
@@ -13,36 +13,13 @@ defmodule Auth2024Web.PageLive do
       user when not is_nil(user) <- Auth2024.Accounts.get_user_by_session_token(token) do
         current_person = Todos.find_person(user)
         items = Todos.list_items(user)
-      {:ok, assign(socket, current_user: user, current_person: current_person, items: items, editing: nil, tab: "all")}
+      {:ok, assign(socket, current_user: user, current_person: current_person, items: items, editing: nil, editing_item_caption: nil, tab: "all")}
     else
       _ -> {:ok, socket}
     end
     #{:ok, socket}
   end
 
-  @impl true
-  def handle_event("create", %{"text" => text}, socket) do
-    Todos.add_item(socket.assigns.current_user, %{caption: text, status: 0, author: socket.assigns.current_person, contact: socket.assigns.current_person})
-    socket = assign(socket, items: Todos.list_items(socket.assigns.current_user), active: %Item{})
-    Auth2024Web.Endpoint.broadcast_from(self(), @topic, "update", socket.assigns)
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_event("toggle", data, socket) do
-    user = socket.assigns.current_user
-	  status = if Map.has_key?(data, "value"), do: 1, else: 0
-	  item = Todos.get_item!(Map.get(data, "id"))
-	  Todos.update_item(user, item, %{status: status})
-	  socket = assign(socket, items: Todos.list_items(user), active: %Item{})
-    Auth2024Web.Endpoint.broadcast(@topic, "update", socket.assigns)
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_event("edit-item", data, socket) do
-    {:noreply, assign(socket, editing: String.to_integer(data["id"]))}
-  end
 
   def do_edit_done(socket, item_id, text) do
     user = socket.assigns.current_user
@@ -56,9 +33,52 @@ defmodule Auth2024Web.PageLive do
 
 
   @impl true
-  def handle_event("update-item", %{"id" => item_id, "text" => text}, socket) do
+  def handle_event("create", %{"text" => text}, socket) do
+    Todos.add_item(socket.assigns.current_user, %{caption: text, status: 0, author: socket.assigns.current_person, contact: socket.assigns.current_person})
+    socket = assign(socket, items: Todos.list_items(socket.assigns.current_user), active: %Item{})
+    Auth2024Web.Endpoint.broadcast_from(self(), @topic, "update", socket.assigns)
+    {:noreply, socket}
+  end
+
+
+  @impl true
+  def handle_event("toggle", data, socket) do
+    user = socket.assigns.current_user
+	  status = if Map.has_key?(data, "value"), do: 1, else: 0
+	  item = Todos.get_item!(Map.get(data, "id"))
+	  Todos.update_item(user, item, %{status: status})
+	  socket = assign(socket, items: Todos.list_items(user), active: %Item{})
+    Auth2024Web.Endpoint.broadcast(@topic, "update", socket.assigns)
+    {:noreply, socket}
+  end
+
+
+  @impl true
+  def handle_event("edit-item", data, socket) do
+    {:noreply, assign(socket, editing: String.to_integer(data["id"]))}
+  end
+
+
+  @impl true
+  def handle_event("submit-todo-item", %{"id" => item_id, "text" => text}, socket) do
     do_edit_done(socket, item_id, text)
   end
+
+
+  @impl true
+  def handle_event("validate-todo-item", %{"_target" => _target, "text" => text}, socket) do
+    IO.inspect(text)
+    {:noreply, assign(socket, editing_item_caption: text)}
+  end
+
+
+  @impl true
+  def handle_event("left-todo-item", _data, socket) do
+    IO.inspect(socket.assigns.editing)
+    do_edit_done(socket, socket.assigns.editing, socket.assigns.editing_item_caption)
+    {:noreply, socket}
+  end
+
 
   @impl true
   def handle_event("delete", data, socket) do
@@ -68,6 +88,7 @@ defmodule Auth2024Web.PageLive do
     Auth2024Web.Endpoint.broadcast(@topic, "update", socket.assigns)
     {:noreply, socket}
   end
+
 
   @impl true
   def handle_params(params, _url, socket) do
@@ -88,14 +109,17 @@ defmodule Auth2024Web.PageLive do
     end
   end
 
+
   @impl true
   def handle_info(%{event: "update", payload: %{items: items}}, socket) do
     {:noreply, assign(socket, items: items)}
   end
 
+
   def checked?(item) do
     not is_nil(item.status) and item.status > 0
   end
+
 
   def completed?(item) do
     if not is_nil(item.status) and item.status > 0, do: "completed", else: ""
