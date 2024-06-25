248913c453fd1402ca14301028d3495ec9c9c12a
Timo Weggen <timo.weggen@rohde-schwarz.com>
2024-06-25 19:15:02 +0200

    - got grouping done.

--- lib/auth2024_web/components/grouping_components.ex
+++ lib/auth2024_web/components/grouping_components.ex
@@ -1,13 +1,28 @@
 defmodule Auth2024Web.GroupingComponent do
   use Phoenix.Component
 
+	def group_by_func(sort_by_column) do
+		case sort_by_column do
+			"date" -> fn (item) -> item.due end
+			"contact" -> fn (item) -> "#{item.contact.given_name} #{item.contact.family_name}" end
+		end
+	end
+
+	def group_by_label(item, sort_by_column) do
+		case sort_by_column do
+			"date" -> item.due
+			"contact" -> item.contact.given_name
+		end
+	end
+
 	slot :inner_block, required: true
 	slot :header
+
 
   def for_items(assigns) do
 		# In this, group the files by date, sorted by id.		
 		item_map = assigns.items
-    |> Enum.group_by(& Map.get(&1, assigns.kind))
+    |> Enum.group_by(group_by_func(assigns.sort_by_column))
     |> Enum.map(fn {attr, items} -> 
       {attr, Enum.sort_by(items, & &1.id)}
     end)
@@ -16,7 +31,7 @@ defmodule Auth2024Web.GroupingComponent do
     ~H"""
     	<%= for {attr, item_list} <- item_map do %>
     		<div>
-      		<%= render_slot(@header, attr) || "attr header" %>
+      		<%= render_slot(@header, attr) || "sort_by_column" %>
     		</div>
     		<%= for item <- item_list do %>
 					<%= render_slot(@inner_block, item) %>
@@ -24,4 +39,5 @@ defmodule Auth2024Web.GroupingComponent do
     	<% end %>
     """
   end
+
 end
