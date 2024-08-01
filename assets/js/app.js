98a8349d20912d9f0ef1df7ffb0240d1be092f6d
Timo Weggen <timo.weggen@rohde-schwarz.com>
2024-08-01 20:21:05 +0200

    - lots of tweaks, no result.

--- assets/js/app.js
+++ assets/js/app.js
@@ -102,5 +102,5 @@ const documentHeight = () => {
  const doc = document.documentElement
  doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
 }
-window.addEventListener(‘resize’, documentHeight)
+window.addEventListener('resize', documentHeight)
 documentHeight()
