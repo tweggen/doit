2e173c93b7dd7efe0835610484ac129e50f4b0f1
Timo Weggen <timo.weggen@rohde-schwarz.com>
2024-08-26 11:29:33 +0200

    - fixed sorting of dates.

--- lib/auth2024_web/components/grouping_components.ex
+++ lib/auth2024_web/components/grouping_components.ex
@@ -19,11 +19,23 @@ defmodule Auth2024Web.GroupingComponent do
 	end
 
 
-	def sort_groups_func(sort_by_column) do
+	def sort_groups_map_func(sort_by_column) do
 		case sort_by_column do
 			"date" -> fn (arg = {attr, _items}) -> attr end
 			"contact" -> fn (arg = {attr, _items}) -> attr.family_name end
 			#_x -> fn (item) -> item.id end
+		end
+	end
+
+
+	def sort_groups_sort_func(sort_by_column) do
+		case sort_by_column do
+			"date" -> fn (a, b) -> 
+				res = Date.compare(a,b)
+				#IO.inspect("comparing #{a} to #{b} results #{res}")
+				res==:lt
+			end
+			"contact" -> fn (a, b) -> a <= b end
 		end
 	end
 
@@ -47,7 +59,7 @@ defmodule Auth2024Web.GroupingComponent do
     |> Enum.map(fn {attr, items} -> 
       {attr, Enum.sort_by(items, sort_items_func(assigns.sort_by_column))}
     end)
-    |> Enum.sort_by(sort_groups_func(assigns.sort_by_column))
+    |> Enum.sort_by(sort_groups_map_func(assigns.sort_by_column), sort_groups_sort_func(assigns.sort_by_column))
     # |> Enum.into(%{})	
     #IO.inspect(item_map)
     ~H"""
