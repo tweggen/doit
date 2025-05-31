7d2ae641eb808794991b8966c078f0a69bdb89f3
tweggen <timo.weggen@gmail.com>
2025-05-31 14:24:50 +0200

    - updated versions and added new visual studio to mixset

--- mixsetup.bat
+++ mixsetup.bat
@@ -2,6 +2,7 @@
 
 rem vc vars
 call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 call "c:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
 echo call complete
